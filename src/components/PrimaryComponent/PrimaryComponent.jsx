86f8e336e46dc21d049313136ba088f9749a471d
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-01-31 03:59:43 +0600

    Create PrimaryComponent and BackdropBlur

---
+++ src/components/PrimaryComponent/PrimaryComponent.jsx
@@ -0,0 +1,43 @@
+// react router import
+import { RouterProvider } from "react-router-dom";
+
+// router
+import router from "../../router/router";
+
+// react toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Slide } from "react-toastify";
+
+// components
+import BackdropBlur from "../shared/BackdropBlur/BackdropBlur";
+
+// styles
+import styles from "./primaryComponent.module.scss";
+
+const PrimaryComponent = () => {
+  return (
+    <div className={`${styles["primary-component"]}`}>
+      {/* react toastify */}
+      <ToastContainer
+        position="top-center"
+        autoClose={2000}
+        transition={Slide}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      {/* blur overlay in the page */}
+      <BackdropBlur openState={open} />
+
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+};
+
+export default PrimaryComponent;
