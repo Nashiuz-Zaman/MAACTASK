c61caa58298c7ebbeb2ab74d08aae294d26917a8
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-01 01:00:54 +0600

    Add interfaceData folder for ui data

---
+++ src/interfaceData/navigationOptions.js
@@ -0,0 +1,9 @@
+export const navOptions = [
+  { id: 0, text: "Login", url: "/login", colorTheme: "primary" },
+  {
+    id: 1,
+    text: "Registration",
+    url: "/registration",
+    colorTheme: "primaryAlt",
+  },
+];
