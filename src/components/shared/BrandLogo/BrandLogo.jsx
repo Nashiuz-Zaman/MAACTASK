eabe3bf18bceb0474bb3311cf86831bc331134e2
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-01 01:01:14 +0600

    Add Brandlogo component to project

---
+++ src/components/shared/BrandLogo/BrandLogo.jsx
@@ -0,0 +1,34 @@
+// react imports
+import PropTypes from "prop-types";
+
+// react router imports
+import { Link } from "react-router-dom";
+
+// style
+import styles from "./Brandlogo.module.scss";
+
+const BrandLogo = ({
+  imageSource,
+  modifyClasses = "",
+  imageModifyClasses = "",
+}) => {
+  return (
+    <div className={`${styles["brand-logo"]}${modifyClasses}`}>
+      <Link className="block w-full h-full" to="/">
+        <img
+          className={`${imageModifyClasses}`}
+          src={imageSource}
+          alt="Website Logo"
+        />
+      </Link>
+    </div>
+  );
+};
+
+BrandLogo.propTypes = {
+  modifyClasses: PropTypes.string,
+  imageSource: PropTypes.string.isRequired,
+  imageModifyClasses: PropTypes.string,
+};
+
+export default BrandLogo;
