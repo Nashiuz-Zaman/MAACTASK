2d0bf58c1ab7dd1046eb821d14b8aea240e047c6
Nashiuz Zaman <nashiuz.zaman@gmail.com>
2024-02-01 00:55:27 +0600

    Add LinkBtn, LoadingSpinner, MobileMenuBtn, MobileMenuCloseBtn, MobileNav in the project

---
+++ src/components/shared/LinkBtn/LinkBtn.jsx
@@ -0,0 +1,63 @@
+// react imports
+import PropTypes from "prop-types";
+
+// react router imports
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
+
+// styles
+import styles from "./LinkBtn.module.scss";
+
+const LinkBtn = ({
+  text,
+  url,
+  colorTheme = "primary",
+  hashed = false,
+  modifyClasses = "",
+  sizeTheme = "base",
+}) => {
+  const colorClasses =
+    colorTheme === "primary"
+      ? styles["primary-btn"]
+      : colorTheme === "primaryAlt"
+      ? styles["primary-btn-alt"]
+      : "";
+
+  const sizeClasses =
+    sizeTheme === "base"
+      ? styles["base-btn"]
+      : sizeTheme === "lg"
+      ? styles["large-btn"]
+      : "";
+
+  const baseClasses = styles["link-btn-container"];
+
+  if (hashed) {
+    return (
+      <div className={`${baseClasses} ${colorClasses} ${modifyClasses}`}>
+        <HashLink className={`${sizeClasses}`} to={url}>
+          {text}
+        </HashLink>
+      </div>
+    );
+  }
+
+  return (
+    <div className={`${baseClasses} ${colorClasses} ${modifyClasses}`}>
+      <Link className={`${sizeClasses}`} to={url}>
+        {text}
+      </Link>
+    </div>
+  );
+};
+
+LinkBtn.propTypes = {
+  text: PropTypes.string,
+  url: PropTypes.string,
+  colorTheme: PropTypes.string,
+  hashed: PropTypes.bool,
+  modifyClasses: PropTypes.string,
+  sizeTheme: PropTypes.string,
+};
+
+export default LinkBtn;
